2530b9a70edf86a7e820f00e3a842ea808f0c944
Tyler Mandry <tmandry@gmail.com>
2018-02-14 13:28:05 -0600

    Remove accessor trait pattern
    This is, apparently, not Rustic. Also, using refs in structs is powerful.

--- src/book.rs
+++ src/book.rs
@@ -57,33 +57,14 @@ impl Order {
     }
 }
 
-pub trait OrderInfo {
-    fn id(&self) -> &str;
-    fn side(&self) -> Side;
-    fn price(&self) -> Price;
-    fn orig_size(&self) -> Price;
-    fn open_size(&self) -> Price;
-}
-
-impl OrderInfo for Order {
-    fn id(&self) -> &str { &self.id }
-    fn side(&self) -> Side { self.side }
-    fn price(&self) -> Price { self.px }
-    fn orig_size(&self) -> Price { self.orig_size }
-    fn open_size(&self) -> Price { self.orig_size }
-}
-
-impl Order {
-    // From<OrderInfo> trait doesn't work, because Order: OrderInfo and this conflicts with the
-    // default reflexive implementation.
-
-    fn from_order_info<O: OrderInfo>(o: &O) -> Self {
+impl<'a> From<&'a NewOrderEvent<'a>> for Order {
+    fn from(o: &NewOrderEvent<'a>) -> Self {
         Order{
-            id: o.id().to_string(),
-            side: o.side(),
-            px: o.price(),
-            orig_size: o.orig_size(),
-            open_size: o.open_size()
+            id: o.order_id.to_owned(),
+            side: o.side,
+            px: o.px,
+            orig_size: o.orig_size,
+            open_size: o.open_size,
         }
     }
 }
@@ -93,45 +74,59 @@ pub struct Time(u64);
 #[derive(Copy, Clone, Debug)]
 pub struct Sequence(u64);
 
-pub trait OrderEvent {
-    fn time(&self) -> Time;
-    fn seq(&self) -> Sequence;
+// Event structs. These include the data that is common to all feeds.
+
+pub struct NewOrderEvent<'a> {
+    time: Time,
+    seq: Sequence,
+    order_id: &'a str,
+    side: Side,
+    px: Price,
+    orig_size: Price,
+    open_size: Price,
 }
 
-pub trait OpenEvent : OrderEvent {
-    fn order_id(&self) -> &str;
-    fn remaining_size(&self) -> Price;
+pub struct OpenEvent<'a> {
+    time: Time,
+    seq: Sequence,
+    order_id: &'a str,
+    remaining_size: Price,
+}
+/*
+pub struct MatchEvent<'a> {
+    time: Time,
+    seq: Sequence,
+    maker_order_id: &'a str,
+    taker_order_id: &'a str,
+    side: Side,
+    price: Price,
+    size: Price,
 }
 
-pub trait MatchEvent : OrderEvent {
-    fn maker_order_id(&self) -> &str;
-    fn taker_order_id(&self) -> &str;
-    fn side(&self) -> Side;
-    fn price(&self) -> Price;
-    fn size(&self) -> Price;
+pub struct ChangeEvent<'a> {
+    time: Time,
+    seq: Sequence,
+    order_id: &'a str,
+    price: Option<Price>,  // None indicates market order
+    old_size_or_funds: Price,
+    new_size_or_funds: Price,
 }
 
-pub trait ChangeEvent : OrderEvent {
-    fn order_id(&self) -> &str;
-    fn price(&self) -> Option<Price>;  // None indicates market order
-    fn old_size_or_funds(&self) -> Price;
-    fn new_size_or_funds(&self) -> Price;
+pub struct DoneEvent<'a> {
+    time: Time,
+    seq: Sequence,
+    order_id: &'a str,
+    reason: DoneReason,
 }
 
 pub enum DoneReason {
     Filled,
     Canceled,
 }
-
-pub trait DoneEvent : OrderEvent {
-    fn order_id(&self) -> &str;
-    //fn remaining_size(&self) -> Price;
-    fn reason(&self) -> &DoneReason;
-}
-
+*/
 pub trait Level3FeedListener {
-    fn on_add<O: OrderInfo>(&mut self, order: &O);
-    fn on_open<E: OpenEvent>(&mut self, event: &E);
+    fn on_add<'a>(&mut self, order: &NewOrderEvent<'a>);
+    fn on_open<'a>(&mut self, event: &OpenEvent<'a>);
     //fn on_match<E: MatchEvent>(&mut self, event: &E);
     //fn on_change<E: ChangeEvent>(&mut self, event: &E);
     //fn on_done<E: DoneEvent>(&mut self, event: &E);
@@ -164,29 +159,28 @@ impl Book {
 }
 
 impl Level3FeedListener for Book {
-    fn on_add<O: OrderInfo>(&mut self, order: &O) {
-        let ord = Rc::new(RefCell::new(Order::from_order_info(order)));
+    fn on_add<'a>(&mut self, event: &NewOrderEvent<'a>) {
+        let ord = Rc::new(RefCell::new(Order::from(event)));
         let entry = {
-            let order = ord.borrow();
-            self.orders.insert(order.id.clone(), ord.clone());
-            match &order.side {
-                &Side::Bid => self.bid.entry(order.px),
-                &Side::Ask => self.ask.entry(order.px),
+            self.orders.insert(event.order_id.to_owned(), ord.clone());
+            match &event.side {
+                &Side::Bid => self.bid.entry(event.px),
+                &Side::Ask => self.ask.entry(event.px),
             }
         };
         entry.or_insert(PriceLevel::new()).add(ord);
     }
 
-    fn on_open<E: OpenEvent>(&mut self, event: &E) {
+    fn on_open<'a>(&mut self, event: &OpenEvent<'a>) {
         let (side, px) = {
-            let mut order = self.orders.get(event.order_id()).expect("Unknown order ID").borrow_mut();
-            order.open_size = event.remaining_size();
+            let mut order = self.orders.get(event.order_id).expect("Unknown order ID").borrow_mut();
+            order.open_size = event.remaining_size;
             (order.side, order.px)
         };
 
         let level = self.price_level_mut(side, px)
                         .expect("Price level with order doesn't exist!");
-        level.open_size += event.remaining_size();
+        level.open_size += event.remaining_size;
     }
 }
 
@@ -196,46 +190,33 @@ mod tests {
 
     fn px(p: f64) -> Price { Price::from(p) }
 
+    fn new_event(order_id: &str, side: Side, px: Price, orig_size: Price) -> NewOrderEvent {
+        NewOrderEvent{ seq: Sequence(0), time: Time(0), order_id, side, px, orig_size, open_size: orig_size }
+    }
+
+    fn open_event(order_id: &str, remaining_size: Price) -> OpenEvent {
+        OpenEvent{ seq: Sequence(0), time: Time(0), order_id, remaining_size }
+    }
+
     #[test]
     fn total_size() {
         let mut book = Book::new();
-        book.on_add(&Order::new("order1".to_string(), Side::Bid, px(10.00), px(100.)));
+        book.on_add(&new_event(&"order1", Side::Bid, px(10.00), px(100.)));
         assert_eq!(px(100.), book.price_level(Side::Bid, px(10.00)).unwrap().total_size());
-        book.on_add(&Order::new("order2".to_string(), Side::Bid, px(10.00), px(90.)));
-        book.on_add(&Order::new("order3".to_string(), Side::Ask, px(10.01), px(90.)));
+        book.on_add(&new_event(&"order2", Side::Bid, px(10.00), px(90.)));
+        book.on_add(&new_event(&"order3", Side::Ask, px(10.01), px(90.)));
         assert_eq!(px(190.), book.price_level(Side::Bid, px(10.00)).unwrap().total_size());
         assert_eq!(px(90.), book.price_level(Side::Ask, px(10.01)).unwrap().total_size());
-    }
-
-    struct MyOpenEvent {
-        time: Time,
-        seq: Sequence,
-        order_id: String,
-        remaining_size: Price,
-    }
-
-    impl MyOpenEvent {
-        fn new(order_id: &str, remaining_size: Price) -> MyOpenEvent {
-            MyOpenEvent{ time: Time(0), seq: Sequence(0), order_id: order_id.to_owned(), remaining_size }
-        }
-    }
-    impl OrderEvent for MyOpenEvent {
-        fn time(&self) -> Time { self.time }
-        fn seq(&self) -> Sequence { self.seq }
-    }
-    impl OpenEvent for MyOpenEvent {
-        fn order_id(&self) -> &str { &self.order_id }
-        fn remaining_size(&self) -> Price { self.remaining_size }
     }
 
     #[test]
     fn open_size() {
         let mut book = Book::new();
-        book.on_add(&Order::new("order1".to_string(), Side::Bid, px(10.00), px(100.)));
+        book.on_add(&new_event(&"order1", Side::Bid, px(10.00), px(100.)));
         assert_eq!(Price::zero(), book.price_level(Side::Bid, px(10.00)).unwrap().open_size());
-        book.on_add(&Order::new("order2".to_string(), Side::Bid, px(10.00), px(90.)));
+        book.on_add(&new_event(&"order2", Side::Bid, px(10.00), px(90.)));
         assert_eq!(Price::zero(), book.price_level(Side::Bid, px(10.00)).unwrap().open_size());
-        book.on_open(&MyOpenEvent::new("order2", px(90.)));
+        book.on_open(&open_event(&"order2", px(90.)));
         assert_eq!(px(90.), book.price_level(Side::Bid, px(10.00)).unwrap().open_size());
     }
 }
